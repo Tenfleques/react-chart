d4be66109b35ae916e6cd93d37949ba7e8864a06
Tendai Chikake <tendaichikae@gmail.com>
2019-03-24 04:21:35 +0300

    Updates

---
+++ precache-manifest.03c662871fce632026f28ac98e4693c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef536b1ad39e623356b6",
+    "url": "/react-chart/static/css/main.e8090421.chunk.css"
+  },
+  {
+    "revision": "ef536b1ad39e623356b6",
+    "url": "/react-chart/static/js/main.65d0bb60.chunk.js"
+  },
+  {
+    "revision": "3b99462e151525fa4388",
+    "url": "/react-chart/static/js/runtime~main.61a9340a.js"
+  },
+  {
+    "revision": "f6d62ed96ebf67df8a29",
+    "url": "/react-chart/static/js/2.10eb3309.chunk.js"
+  },
+  {
+    "revision": "4e2989cd80d4d9b6b6e80d424e72db12",
+    "url": "/react-chart/index.html"
+  }
+];
